4553b6b4c6382ef80a92d8c279979670871585fd
lxy123hh <1677837750@qq.com>
2025-04-22 16:52:37 +0800

    feat:learn js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-console.log(`
-    alskdng
-    asdfnasdjldb
-    asdvnaj
-    aasvjngjnvkjba
-    avnkja
-    aldsjngfaghj`)
+// 1.打印
+console.log("hello world")
+
+// 2.全局对象
+console.log(globalThis)
+
+// 3.注释
+/**
+ * 多行注释
+ * 
+ */
+
+console.log(/*等待传入的参数*/"hello world")
+
+// 4.严格模式
