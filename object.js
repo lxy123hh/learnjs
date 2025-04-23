665a7228fcce3fd1427a339c2cd929175515b038
lxy123hh <1677837750@qq.com>
2025-04-23 17:25:25 +0800

    feat:learn object

--- object.js
+++ object.js
@@ -4,7 +4,7 @@
 var obj = {
     name:'zzz',
     age:'18'
- }
+ } //简单对象
  //增加属性
  obj.sex='n'
  //读取属性
@@ -22,4 +22,8 @@ console.log(obj)
 // symbol对象
 var key = Symbol('key')
 obj[key] = 'this is symbol'
+//取值
 console.log(obj[key])
+
+//私有属性，设定一个symbol对象，但不给key变量，这个属性谁也无法获取
+//面向对象编程，封装（数据、方法）
