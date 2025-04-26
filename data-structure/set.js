fbd0a626e296c7eea0850a53839ffd91793bb420
lxy123hh <1677837750@qq.com>
2025-04-26 19:06:00 +0800

    feat:learn set

--- data-structure/set.js
+++ data-structure/set.js
@@ -7,4 +7,9 @@ console.log(set)
 
 const arr = [1,2,2,2,2,2,3]
 const set1 = new Set(arr)
-console.log(set1)
+const arr2 = Array.from(set1)
+console.log(arr2)
+
+let arr3 = [1, 2, 3, 4, 5, 1, 2];
+let uniqueArr = [...new Set(arr3)];
+console.log(uniqueArr); // [1, 2, 3, 4, 5]
