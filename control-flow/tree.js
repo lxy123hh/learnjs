9455e0c9907311a07d652699f71cc4471848abd3
lxy123hh <1677837750@qq.com>
2025-04-26 20:25:03 +0800

    feat:learn tree

---
+++ control-flow/tree.js
@@ -0,0 +1,16 @@
+const treeNode = {
+    val: 3,
+    left:null,
+    right:null
+}
+
+const treeNode2= {
+    val: 3,
+    left:null,
+    right:null
+}
+const treeRoot = {
+    val: 3,
+    left:treeNode,
+    right:treeNode2
+}
