13b2bae8ab03c2ccda52422e7ac419686f988a25
lxy123hh <1677837750@qq.com>
2025-04-26 20:49:39 +0800

    feat:learn function

---
+++ function/index.js
@@ -0,0 +1,14 @@
+function test() {
+    console.log('test');
+    return 'hello'
+}
+
+const fn = function test() {
+    console.log('test');
+    return 'hello'
+}
+//匿名函数
+const fn1 = function (){
+    console.log('test');
+}
+fn()
