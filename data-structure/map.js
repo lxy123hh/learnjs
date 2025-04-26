8738acd54fdcfb0e5ce9f32a63bbdb01f406e694
lxy123hh <1677837750@qq.com>
2025-04-26 19:13:35 +0800

    feat:learn map

---
+++ data-structure/map.js
@@ -0,0 +1,14 @@
+let map = new Map([
+    ['name', 'Tom'],
+    ['age', 25],
+    ['job', 'developer']
+  ]);
+  
+  console.log(map); // Map { 'name' => 'Tom', 'age' => 25, 'job' => 'developer' }
+
+
+  const hashmap = new Map()
+  hashmap.set('name','zhangsan')
+  hashmap.set('age','18')
+  hashmap.set('name','lisi')
+  console.log(hashmap); 
