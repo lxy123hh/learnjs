2176c6bb70797b77c28ba2a9523522c433147302
lxy123hh <1677837750@qq.com>
2025-04-29 16:25:27 +0800

    feat:learn map

--- inner-method/array.js
+++ inner-method/array.js
@@ -1,5 +1,5 @@
 const arr = [1, 2, 3, 4, 5];
-const arr1 = [{name: 'Tom'}, {name: 'Jerry'}, {name: 'Jack'}];
+const arr1 = [{name: 1}, {name: 2}, {name:3}];
 // 1.数组遍历
 // const callback = function(a, b, arr) {
 //   console.log(a, b, arr);
@@ -9,7 +9,7 @@ const arr1 = [{name: 'Tom'}, {name: 'Jerry'}, {name: 'Jack'}];
 //引用类型在做copy时，只是copy了引用地址，浅拷贝
 //深拷贝：新建一个对象，把原对象的属性值copy过来
 //const test = arr1[0];  //浅拷贝
-const test = {
+const test = {                         //深拷贝
     name: arr1[0].name,
 }
 test.name = 2;
@@ -17,6 +17,20 @@ test.name = 2;
 
 
 arr1.forEach(function(item) {
+    //如果原数组中的item是引用类型，直接修改item的属性值会影响原数组中的item
+    //如果原数组中的item是基本类型，直接修改item的属性值不会影响原数组中的item
+    //一般情况下不这么做
+    item.name = item.name + 1;
     console.log(item.name);
 })
+console.log(arr1); 
 
+
+
+//2.数组的map方法
+const newarr1 = arr1.map(function(item) {
+    console.log(item)
+    return {name:'lxy'}
+})
+console.log(newarr1); 
+console.log(arr1); //原数组不变
