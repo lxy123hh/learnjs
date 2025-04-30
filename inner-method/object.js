659ff3682da6b30ac61710cfa2919fa00f780cc4
lxy123hh <1677837750@qq.com>
2025-04-30 15:09:36 +0800

    feat:learn freeze

--- inner-method/object.js
+++ inner-method/object.js
@@ -16,5 +16,12 @@ console.log(target) //{ name: 3, age: 4 }，target被修改了，source不变
 const result1 = Object.entries(target) //把对象转换成数组，返回一个二维数组
 console.log(result1) // [ [ 'name', 3 ], [ 'age', 4 ] ]
 result1.forEach(function(item) {
-    console.log(item[0], item[1]) //打印每个属性名和属性值
-}) //name 3 age 4
+    return '${item[0]} : ${item[1]}' //打印每个属性名和属性值
+})
+
+//3.freeze 方法,冻结对象，不能修改对象的属性值
+const obj = { name: 'Tom' };
+Object.freeze(obj); // 冻结对象
+obj.name = 'Jerry'; // 尝试修改属性值
+// console.log(obj.name); // Tom，属性值没有被修改
+// obj.age = 18; // 尝试添加新属性
