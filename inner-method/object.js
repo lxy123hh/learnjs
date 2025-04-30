e95af1dbe60639bd6968dd41a7b3fc7eed11c7c8
lxy123hh <1677837750@qq.com>
2025-04-30 15:04:13 +0800

    feat:learn object

--- inner-method/object.js
+++ inner-method/object.js
@@ -11,3 +11,10 @@ const source = {
 const result = Object.assign(target, source) //把source的属性复制到target中，返回target
 console.log(result) //{ name: 3, age: 4 }，target被修改了，source不变
 console.log(target) //{ name: 3, age: 4 }，target被修改了，source不变
+
+//2.entries
+const result1 = Object.entries(target) //把对象转换成数组，返回一个二维数组
+console.log(result1) // [ [ 'name', 3 ], [ 'age', 4 ] ]
+result1.forEach(function(item) {
+    console.log(item[0], item[1]) //打印每个属性名和属性值
+}) //name 3 age 4
